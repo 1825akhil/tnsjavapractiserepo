95339922620a88b05dbf3912f265adc7045149ad
1825akhil <1825akhil@gmail.com>
2025-09-22 18:02:41 +0530

    21/09/2025 - Task

---
+++ septtwentyone/ArrayException.java
@@ -0,0 +1,15 @@
+package com.tns.septtwentyone;
+
+public class ArrayException {
+
+	public static void main(String[] args) {
+		String str = "Akhil";
+		try {
+		 System.out.println(str.charAt(5));	
+		 }catch(Exception e) {
+			 System.out.println(e);
+		 }
+
+	}
+
+}
