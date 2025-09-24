4cd6a0f8f87ae67c87674c7726e46d7339c45822
1825akhil <1825akhil@gmail.com>
2025-09-25 00:52:38 +0530

    24/09/2025 - Task

---
+++ septtwentyfour/JoinMain.java
@@ -0,0 +1,20 @@
+package com.tns.septtwentyfour;
+
+public class JoinMain {
+
+	public static void main(String[] args) {
+		for(int i =0;i<3;i++) {
+		JoinMethod obj = new JoinMethod(i);
+		Thread myThread = new Thread(obj);
+		myThread.start();
+		try {
+			myThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+
+	}
+
+}
