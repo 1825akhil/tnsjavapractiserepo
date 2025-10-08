6676d753c7d43dc8b41f02a348a017051fc9428c
1825akhil <1825akhil@gmail.com>
2025-10-09 00:31:03 +0530

    Lambda programs

---
+++ lambda/LambdaDemo.java
@@ -0,0 +1,7 @@
+package com.tns.lambda;
+
+@FunctionalInterface
+public interface LambdaDemo {
+
+	public void show();
+}
---
+++ lambda/LambdaMain.java
@@ -0,0 +1,11 @@
+package com.tns.lambda;
+
+public class LambdaMain {
+
+	public static void main(String[] args) {
+		LambdaDemo obj = () -> System.out.println("This is the show method");
+		obj.show();
+
+	}
+
+}
